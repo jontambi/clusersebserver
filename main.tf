da5513052ee8ba4ff288e7aea4dc9b486ae0c712
John Tambi <jon.tambi@gmail.com>
2019-08-16 22:41:59 -0500

    ClusterWebserver Initial configuration

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_launch_configuration" "example" {
+    image_id             = "ami-07d0cf3af28718ef8"
+    instance_type        = "t2.micro"
+    security_groups      = ["${aws_security_group.intance.id}"]
+
+    user_data = <<-EOF
+              #!/bin/bash
+                 echo "Hello, World" > index.html
+                 nohup busybox httpd -f -p "${var.server_port}" &
+                 EOF
+
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+      from_port   = "${var.server_port}"
+      to_port     = "${var.server_port}"
+      protocol    = "tcp"
+      cdir_clocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+      create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  
+}
+
